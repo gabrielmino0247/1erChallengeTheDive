03f91e1586618a0749df8ad62a404d2cc5fec6b2
Gabriel Mino <angel.mino.0024@gmail.com>
2025-05-29 16:46:36 -0300

    se optimizo lo  mas que se pudo y se arreglaron errores muy puntuales, de aqui en mas ya es funcional, solo un problema persiste, al finalizar se imprime doble el tablero

--- app.py
+++ app.py
@@ -32,9 +32,12 @@ CREATE TABLE IF NOT EXISTS partidas (
 )
 """)
 
-conexion.commit()
+conexion.commit()  #el commit es como en git, guarda los cambios en este caso en la bbdd
 
 def guardar_movimiento_sql(cursor, jugada, id_partida):
+    '''
+    Guarda un movimiento en la base de datos
+    '''
     cursor.execute("""
         INSERT INTO movimientos (
             id_partida, turno, jugador, tipo_control,
@@ -59,9 +62,8 @@ class Juego:
         self.dimensiones = dimensiones
         self.gato = gato_pos
         self.raton = raton_pos
-        # El tablero se crea en función del estado actual
-        self.tablero = [['.' for _ in range(self.dimensiones[1])] for _ in range(self.dimensiones[0])]
-        self.obstaculos = self.generar_obstaculos(12)
+        self.tablero = [['.' for _ in range(self.dimensiones[1])] for _ in range(self.dimensiones[0])] #forma muy eficiente de crear el tablero
+        self.obstaculos = self.generar_obstaculos(12) 
         for obs in self.obstaculos:
             self.tablero[obs[0]][obs[1]] = '█'
         self.tablero[self.gato[0]][self.gato[1]] = 'G'
@@ -70,16 +72,17 @@ class Juego:
     def copiar_estado(self):
         # Esta es la forma correcta de copiar el estado: crea una nueva instancia con los mismos datos
         nuevo_juego = Juego(self.gato, self.raton, self.dimensiones)
-        nuevo_juego.turnos = self.turnos # ¡IMPORTANTE! Copiar también el contador de turnos
+        nuevo_juego.turnos = self.turnos # se debe copiar SI O SIII esta parte
         return nuevo_juego
     
     def es_posicion_valida(self, pos):
-        """Verifica si la posición está dentro del tablero y no hay un obstáculo."""
+        '''Verifica si la posición está dentro del tablero y no hay un obstáculo.'''
         dentro_del_tablero = 0 <= pos[0] < self.dimensiones[0] and 0 <= pos[1] < self.dimensiones[1]
         libre_de_obstaculo = pos not in self.obstaculos
         return dentro_del_tablero and libre_de_obstaculo
     
     def generar_obstaculos(self, cantidad):
+        '''Genera una cantidad elegida de obstáculos en posiciones aleatorias del tablero.'''
         obstaculos = set()
         while len(obstaculos) < cantidad:
             fila = random.randint(0, self.dimensiones[0] - 1)
@@ -92,22 +95,27 @@ class Juego:
         return list(obstaculos)
 
     def max_turnos(self):
-        return ((self.dimensiones[0] + self.dimensiones[1]) // 2 ) #3 movimientos de gracia para que sea mas dinamico
-
+        return ((self.dimensiones[0] + self.dimensiones[1]) // 2 ) # Definimos una cantidad máxima de turnos como la mitad del perímetro del tablero para que no se incline  siempre hacia algun personaje
+    
     def imprimir_tablero(self):
         for obs in self.obstaculos:
             self.tablero[obs[0]][obs[1]] = '█'
         for fila in self.tablero:
-            print(' '.join(fila)) # Uso ' ' en lugar de '.' para mejor legibilidad
+            print(' '.join(fila)) #sirve como unircadenas en EXCEL 
         print(f"Gato en: {self.gato}, Ratón en: {self.raton}")
     
+    def imprimir_tablero_final(self):
+        for obs in self.obstaculos:
+            self.tablero[obs[0]][obs[1]] = '█'
+        for fila in self.tablero:
+            print(' '.join(fila))  #sirve como unircadenas en EXCEL 
+
     def mover_gato(self, nueva_pos, ruido=False):
         if self.es_posicion_valida(nueva_pos):
             self.tablero[self.gato[0]][self.gato[1]] = '.'
             self.gato = nueva_pos
-
             if self.gato == self.raton:
-                self.tablero[self.gato[0]][self.gato[1]] = 'X'  # Gato atrapó al ratón
+                self.tablero[self.gato[0]][self.gato[1]] = 'X'  
             else:
                 self.tablero[self.gato[0]][self.gato[1]] = 'G'
         else:
@@ -123,17 +131,17 @@ class Juego:
                 self.tablero[self.raton[0]][self.raton[1]]= 'X'
         else:
             if not ruido:
-                print("Movimiento inválido para el ratón.")
+                print("Movimiento inválido para el ratón.")   #este era el mensaje para testear cuantas veces se llamaba y salia mal su movimiento, pero para el juego se usa ruido true para que no se vea
 
     def obtener_posibles_movimientos(self, personaje_pos, es_raton=False):
         movimientos_posibles = []
         
-        # Define los movimientos para el gato (4 direcciones)
+        # Define los movimientos para el gato en 8 direcciones
         direcciones_gato = [(-1, 0), (1, 0), (0, -1), (0, 1),  # Cardinales
-                             (-1, -1), (-1, 1), (1, -1), (1, 1)
-                            ]  # Diagonales   
+                             (-1, -1), (-1, 1), (1, -1), (1, 1)  # Diagonales
+                            ]    
 
-        # Define los movimientos para el ratón (8 direcciones)
+        # Define los movimientos para el ratón en 8 direcciones
         direcciones_raton = [
             (-1, 0), (1, 0), (0, -1), (0, 1),  # Cardinales
             (-1, -1), (-1, 1), (1, -1), (1, 1)  # Diagonales
@@ -147,40 +155,37 @@ class Juego:
                 movimientos_posibles.append(nueva_pos)
         return movimientos_posibles
 
-    def es_gato_ganador(self):
-        # El gato gana si su posición es igual a la del ratón
+    def es_gato_ganador(self): # El gato gana si su posición es igual a la del ratón
         return self.gato == self.raton
     
-    def es_raton_ganador(self):
-        # El ratón gana si sobrevive la cantidad máxima de turnos sin ser atrapado
+    def es_raton_ganador(self):  # El ratón gana si sobrevive la cantidad máxima de turnos sin ser atrapado
         return self.turnos >= self.max_turnos() and not self.es_gato_ganador()
 
-    def es_fin_de_juego(self):
-        # El juego termina si el gato gana O el ratón gana
+    def es_fin_de_juego(self): # El juego termina si el gato gana O el ratón gana
         return self.es_gato_ganador() or self.es_raton_ganador()    
 
     def evaluar_estado(self):
-        # Puntuaciones terminales:
         if self.es_gato_ganador():
             return 1000  # Puntuación muy alta para el gato (maximizador)
         
         if self.es_raton_ganador():
             return -1000 # Puntuación muy baja para el gato (victoria del ratón)
 
-        # Heurística para estados intermedios:
-        # Usamos la distancia de Manhattan: abs(x1-x2) + abs(y1-y2)
-        # Cuanto menor sea la distancia entre gato y ratón, mejor para el gato.
-        # Por lo tanto, regresamos un valor negativo de la distancia, así Minimax
-        # intentará MINIMIZAR la distancia (que se traduce en MAXIMIZAR -distancia)
+        '''Heurística para estados intermedios:
+        Usamos la distancia de Manhattan: abs(x1-x2) + abs(y1-y2)
+        Cuanto menor sea la distancia entre gato y ratón, mejor para el gato.
+        Por lo tanto, regresamos un valor negativo de la distancia, así Minimax
+        intentará MINIMIZAR la distancia (que se traduce en MAXIMIZAR - distancia)
+        '''
         distancia = abs(self.gato[0] - self.raton[0]) + abs(self.gato[1] - self.raton[1])
         return -distancia
     
     def minimax(self, estado, profundidad, es_turno_maximizador):
-        # 1. Casos Base: Si el juego ha terminado o hemos llegado a la profundidad límite
+        #1 Casos Base: Si el juego ha terminado o hemos llegado a la profundidad límite
         if estado.es_fin_de_juego() or profundidad == 0:
             return estado.evaluar_estado()
 
-        # 2. Turno del Jugador Maximizador (Gato)
+        #2 Turno del Jugador Maximizador (Gato)
         if es_turno_maximizador: # El gato quiere maximizar la puntuación
             mejor_valor = float('-inf') # Inicializar con el valor más bajo posible
             
@@ -195,7 +200,7 @@ class Juego:
                 mejor_valor = max(mejor_valor, valor) # El gato elige el movimiento que da la puntuación más alta
             return mejor_valor
 
-        # 3. Turno del Jugador Minimizador (Ratón)
+        #3 Turno del Jugador Minimizador (Ratón)
         else: # El ratón quiere minimizar la puntuación (del gato)
             mejor_valor = float('inf') # Inicializar con el valor más alto posible
             
@@ -213,7 +218,7 @@ class Juego:
     
     def obtener_mejor_movimiento_gato(self, profundidad):
         mejor_valor = float('-inf')
-        mejores_movimientos = [] # Cambiar de un solo movimiento a una lista
+        mejores_movimientos = [] # cambiar a una lista para tener múltiples movimientos
         
         for mov_gato in self.obtener_posibles_movimientos(self.gato, es_raton=False):
             estado_despues_mov_gato = self.copiar_estado()
@@ -233,7 +238,7 @@ class Juego:
 
     def obtener_mejor_movimiento_raton(self, profundidad):
         mejor_valor = float('inf')
-        mejores_movimientos = [] # Cambiar a una lista
+        mejores_movimientos = [] # Cambiar a una lista para tener múltiples movimientos
 
         for mov_raton in self.obtener_posibles_movimientos(self.raton, es_raton=True):
             estado_despues_mov_raton = self.copiar_estado()
@@ -270,7 +275,7 @@ def leer_movimiento_usuario(pos_actual):
             nueva_pos = (pos_actual[0] + dx, pos_actual[1] + dy)
             return nueva_pos
         else:
-            print("Tecla inválida. Usá W, A, S, D, Q, E, Z o C.")
+            print("Tecla inválida. Usa W, A, S, D, Q, E, Z o C.")
 
 def mostrar_estadisticas():
     import sqlite3
@@ -280,11 +285,11 @@ def mostrar_estadisticas():
     print("\nEstadísticas de Partidas\n")
 
     cursor.execute("SELECT COUNT(*) FROM partidas")
-    total = cursor.fetchone()[0]
+    total = cursor.fetchone()[0]   #El método devuelve un solo registro o Ninguno si no hay más filas disponibles. y como calculamos el conteo de partidas, siempre habrá al menos una fila
     print(f"Total de partidas registradas: {total}")
 
     cursor.execute("SELECT resultado, COUNT(*) FROM partidas GROUP BY resultado")
-    for resultado, cantidad in cursor.fetchall():
+    for resultado, cantidad in cursor.fetchall(): #fetchall() devuelve una lista de tuplas, donde cada tupla representa una fila del resultado de la consulta
         jugador = "Ratón" if resultado == "raton" else "Gato"
         print(f"Ganadas por {jugador}: {cantidad}")
 
@@ -306,6 +311,14 @@ def mostrar_estadisticas():
     conexion.close()
     input("\nPresioná Enter para volver al menú principal...")
 
+def finalizar_partida(juego, resultado, id_partida, cursor, conexion):
+    juego.imprimir_tablero_final()
+    print("El ratón ha sobrevivido!!! GANA EL RATÓN." if resultado == "raton" else "El gato ha atrapado al ratón!!! GANA EL GATO.")
+    cursor.execute("""
+        UPDATE partidas SET resultado = ?, turnos_totales = ? WHERE id = ?
+    """, (resultado, juego.turnos, id_partida))
+    conexion.commit()
+    conexion.close()
 
 
 def jugar_partida_minimax(modo):
@@ -316,17 +329,17 @@ def jugar_partida_minimax(modo):
     raton_pos = (5, 5)
     juego = Juego(gato_pos, raton_pos)
 
-    profundidad_busqueda = 4 # Puedes ajustar esto. Mayor profundidad = más "inteligente" pero más lento.
+    profundidad_busqueda = 4 # se puede ajustar esto pero mientras mayor sea la profundidad = más "inteligente" pero más lento.
     modo_juego = {
     "1": "IA vs IA",
     "2": "Usuario (ratón) vs IA",
     "3": "Usuario (gato) vs IA"
-    }[modo]
+    }[modo] #sirve solo para el sql
     cursor.execute("""
     INSERT INTO partidas (modo, resultado, turnos_totales, fecha)
     VALUES (?, ?, ?, ?)
-    """, (modo_juego, "", 0, datetime.now().isoformat()))
-    id_partida = cursor.lastrowid
+    """, (modo_juego, "", 0, datetime.now().isoformat()))  #no sabia que se podria usar las variables en las sentencias sql de esta manera
+    id_partida = cursor.lastrowid #lastrowid obtiene el último ID insertado en la tabla, que es el ID de la partida recién creada
     print("--- comienza el juego ---")
     print("\nTablero de Juego:")
     if modo == "1":
@@ -336,14 +349,18 @@ def jugar_partida_minimax(modo):
         juego.imprimir_tablero()
         print(f"Turno: {juego.turnos}")
         
-        # --- TURNO DEL RATÓN ---
+        # Usuario juega como ratón
         if modo == "2": 
-            print("\nTurno del Ratón:") # Usuario juega como ratón
+            print("\nTurno del Ratón:") 
             while True:
                 desde= juego.raton
                 nueva_pos = leer_movimiento_usuario(juego.raton)
                 if juego.es_posicion_valida(nueva_pos):
                     juego.mover_raton(nueva_pos)
+                    juego.imprimir_tablero()
+                    if juego.es_fin_de_juego():
+                        finalizar_partida(juego, "raton" if juego.es_raton_ganador() else "gato", id_partida, cursor, conexion)
+                        return
                     registro_partida.append({
                         "turno": juego.turnos,
                         "jugador": "raton",
@@ -356,7 +373,9 @@ def jugar_partida_minimax(modo):
                     break
                 else:
                     print("Movimiento inválido. Intentalo de nuevo.")
-        else:
+
+        # IA juega como ratón
+        elif modo == "1":
             print("Ratón (IA) pensando...")
             mov_raton = juego.obtener_mejor_movimiento_raton(profundidad_busqueda)
             if mov_raton is None:
@@ -365,6 +384,10 @@ def jugar_partida_minimax(modo):
             desde = juego.raton
             juego.mover_raton(mov_raton, ruido=True)
             print(f"Ratón se mueve a: {mov_raton}")
+            juego.imprimir_tablero()
+            if juego.es_fin_de_juego():
+                finalizar_partida(juego, "raton" if juego.es_raton_ganador() else "gato", id_partida, cursor, conexion)
+                return
             registro_partida.append({
                 "turno": juego.turnos,
                 "jugador": "raton",
@@ -375,27 +398,26 @@ def jugar_partida_minimax(modo):
             })
             guardar_movimiento_sql(cursor, registro_partida[-1], id_partida)
 
-        if juego.es_fin_de_juego():
-            juego.imprimir_tablero()
-            print("¡El ratón ha sobrevivido! GANA EL RATÓN." if juego.es_raton_ganador() else "¡El gato ha atrapado al ratón! GANA EL GATO.")
-            break
+        # if juego.es_fin_de_juego():
+        #     juego.imprimir_tablero()
+        #     print("¡El ratón ha sobrevivido! GANA EL RATÓN." if juego.es_raton_ganador() else "¡El gato ha atrapado al ratón! GANA EL GATO.")
+        #     break
 
-        # --- TURNO DEL GATO ---
+        # Usuario juega como gato
         if modo == "3":
-            print("\nTurno del Gato:")  # Usuario juega como gato
+            print("\nTurno del Gato:")  
+            if juego.es_fin_de_juego():
+                finalizar_partida(juego, "raton" if juego.es_raton_ganador() else "gato", id_partida, cursor, conexion)
+                return
             while True:
-                desde = juego.gato  # 👈 guardamos posición antes de mover
+                desde = juego.gato #guardar la posicion del gato antes de mover y darle otro valor
                 nueva_pos = leer_movimiento_usuario(desde)
                 if juego.es_posicion_valida(nueva_pos):
                     juego.mover_gato(nueva_pos)
+                    juego.imprimir_tablero()
                     if juego.es_fin_de_juego():
-                        juego.imprimir_tablero()
-                        print("¡El ratón ha sobrevivido! GANA EL RATÓN." if juego.es_raton_ganador() else "¡El gato ha atrapado al ratón! GANA EL GATO.")
-                        resultado = "gato"
-                        cursor.execute("""UPDATE partidas SET resultado=?, turnos_totales=? WHERE id=?""", (resultado, juego.turnos, id_partida))
-                        conexion.commit()
-                        conexion.close()
-                        return
+                       finalizar_partida(juego, "gato", id_partida, cursor, conexion)
+                       return
                     registro_partida.append({
                         "turno": juego.turnos,
                         "jugador": "gato",
@@ -408,7 +430,9 @@ def jugar_partida_minimax(modo):
                     break
                 else:
                     print("Movimiento inválido. Intentalo de nuevo.")
-        else:
+        
+        # IA juega como gato
+        elif modo == "1":
             print("Gato (IA) pensando...")
             mov_gato = juego.obtener_mejor_movimiento_gato(profundidad_busqueda)
             if mov_gato is None:
@@ -418,13 +442,9 @@ def jugar_partida_minimax(modo):
             juego.mover_gato(mov_gato, ruido=True)
             print(f"Gato se mueve a: {mov_gato}")
             if juego.es_fin_de_juego():
-                juego.imprimir_tablero()
-                print("¡El ratón ha sobrevivido! GANA EL RATÓN." if juego.es_raton_ganador() else "¡El gato ha atrapado al ratón! GANA EL GATO.")
                 resultado = "raton" if juego.es_raton_ganador() else "gato"
-                cursor.execute("""UPDATE partidas SET resultado=?, turnos_totales=? WHERE id=?""", (resultado, juego.turnos, id_partida))
-                conexion.commit()
-                conexion.close()
-                return  
+                finalizar_partida(juego, resultado, id_partida, cursor, conexion)
+                return
             registro_partida.append({
                 "turno": juego.turnos,
                 "jugador": "gato",
@@ -436,48 +456,40 @@ def jugar_partida_minimax(modo):
             guardar_movimiento_sql(cursor, registro_partida[-1], id_partida)
 
         
-        if juego.es_fin_de_juego():
-            juego.imprimir_tablero()
-            print("¡El gato ha atrapado al ratón! GANA EL GATO." if juego.es_gato_ganador() else "¡El ratón ha sobrevivido! GANA EL RATÓN.")
-            break
+        # if juego.es_fin_de_juego():
+        #     juego.imprimir_tablero()
+        #     print("¡El gato ha atrapado al ratón! GANA EL GATO." if juego.es_gato_ganador() else "¡El ratón ha sobrevivido! GANA EL RATÓN.")
+        #     break
 
         
-        juego.turnos += 1
+        juego.turnos += 1  #esto es lo mas importante del ciclo, para que sume el turno y no se quede en 0 siempre
 
 
-    resultado = "gato" if juego.es_gato_ganador() else "raton"
-    cursor.execute("""
-        UPDATE partidas
-        SET resultado = ?, turnos_totales = ?
-        WHERE id = ?
-    """, (resultado, juego.turnos, id_partida))
-    conexion.commit()
-    conexion.close()
-
-
+#hacemos un menu para que sea mas facil de jugar y poder hacer las consultas en la bd sin hacer mucho rollo en el def del juego principal
 def menu_principal():
     while True:
-        print("\n--- LABERINTO DEL GATO Y EL RATÓN ---")
-        print("1. IA vs IA")
-        print("2. Usuario (ratón) vs IA (gato)")
-        print("3. Usuario (gato) vs IA (ratón)")
-        print("4. Ver estadísticas")
-        print("5. Salir")
+        '''
+        \n--- LABERINTO DEL GATO Y EL RATÓN ---
+        1. IA vs IA
+        2. Usuario (ratón) vs IA (gato)
+        3. Usuario (gato) vs IA (ratón)
+        4. Ver estadísticas
+        5. Salir
+        '''
 
-        opcion = input("Seleccioná una opción (1 a 5): ").strip()
+        opcion = input("\nSeleccioná una opción (1 a 5): ").strip()
 
-        if opcion in ["1", "2", "3"]:
+        if opcion in ["1", "2", "3"]:  ##revisar porque siempre al final imprime dos veces el tablero
             jugar_partida_minimax(opcion)
         elif opcion == "4":
             mostrar_estadisticas()
         elif opcion == "5":
-            print("¡Gracias por jugar!")
+            print("\n\n Gracias por jugar!!!!\n\n\n")
             break
         else:
-            print("Opción inválida.")
+            print("\nOpción inválida.\n")
 
 
 
 # para ejecutar el juego desde el menu
-if __name__ == "__main__":
-    menu_principal()
+menu_principal()
